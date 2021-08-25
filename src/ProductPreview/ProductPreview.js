e044009e0396294e67afb06d69a408e81aad272b
StavRabinovich <stavrab@gmail.com>
2021-08-25 19:31:48 +0300

    v2

---
+++ src/ProductPreview/ProductPreview.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from './ProductPreview.module.css';
+
+const ProductPreview = (props) => {
+  const currentHours = new Date().getHours() > 9 ? new Date().getHours() : '0' + new Date().getHours();
+  const currentMinutes = new Date().getMinutes() > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes();
+
+  return(
+    <div className={classes.ProductPreview}>
+      <img src={props.currentPreviewImage} alt="Product Preview" />
+          {
+            props.HeartBeatsSection ? 
+              <div className={[classes.FeatureData, classes.HeartBeatsSection].join(' ')}>
+                <i class="fas fa-heartbeat"></i>
+                <p>78</p>
+              </div>
+            :
+              <div className={[classes.FeatureData, classes.TimeSection].join(' ')}>
+                <p>{`${currentHours}:${currentMinutes}`}</p>
+              </div>
+          }
+    </div>
+  );
+}
+
+export default ProductPreview;
---
+++ src/Topbar/Topbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Topbar.module.css'
+
+const Topbar = () => {
+  return(
+    <header>
+      <nav className={classes.Topbar}>
+        <img src="https://www.newskarnataka.com/storage/photos/shares/amazon_mns_05012021.jpg" alt="Amazon Logo"></img>
+      </nav>
+    </header>
+    
+  ); 
+}
+export default Topbar;
